31f6f639fcef3c2a91729a3efe2d6f3a8b1aaaec
sanjiv sahayam <sanjsmailbox@gmail.com>
2019-03-29 00:42:23 +1100

    Added a hundred tests and a hundred assertions

---
+++ core/src/test/scala/example/HundredAssertionsSuite.scala
@@ -0,0 +1,19 @@
+package example
+
+import boon._
+import syntax._
+
+object HundredAssertionsSuite extends SuiteLike("HundredAssertionsSuite") {
+
+  private def mkAssertion(index: Int) = {
+    if (index % 2 == 0) true =?= true | s"a truism ${index}"
+    else false =/= true | s"a falsism ${index}"
+  }
+
+  private val t1 = test("A hundred assertions") {
+    val assertions: ContinueSyntax = oneOrMore(1, 2 to 100:_*).map(mkAssertion)
+    assertions
+  }
+
+  override val tests = one(t1)
+}
