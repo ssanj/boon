dcef60de9bea5fee308af9f03ad00b926c3e9d83
sanjiv sahayam <sanjsmailbox@gmail.com>
2019-03-30 22:17:35 +1100

    Add an example of combined syntax

--- core/src/test/scala/example/ListSuite.scala
+++ core/src/test/scala/example/ListSuite.scala
@@ -6,8 +6,12 @@ import syntax._
 object ListSuite extends SuiteLike("ListSuite") {
 
   private val t1 = test("match contents") {
-    List(1, 2, 3, 4, 5) =/= List(2, 4, 6, 8, 10)               | "list contents" and
-    List(1, 2, 3, 4, 5).length =?= List(2, 4, 6, 8, 10).length | "list length"
+    List(1, 2, 3, 4, 5) =/= List(2, 4, 6, 8, 10) | "list contents" and
+    List(1, 2, 3, 4, 5).length =?= 5             | "list length" and
+    //combining assertions for a single object
+    %@(List(1, 2, 3, 4, 5)) { list =>
+      list.sum =?= 15 | "list sum" and list.take(2) =?= List(1,2) | "list take"
+    }
   }
 
   override val tests = one(t1)
