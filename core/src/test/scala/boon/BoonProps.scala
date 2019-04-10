d238490605c2e9182922f5cb1860fc7518700fd5
sanjiv sahayam <sanjsmailbox@gmail.com>
2019-04-10 23:32:25 +1000

    Add test for defineAssertion

--- core/src/test/scala/boon/BoonProps.scala
+++ core/src/test/scala/boon/BoonProps.scala
@@ -4,6 +4,7 @@ import model.IsEqual
 import model.IsNotEqual
 import model.EqualityType
 import model.StringRep
+import model.Testable
 
 import org.scalacheck.Gen
 import org.scalacheck.Prop._
@@ -21,13 +22,27 @@ object BoonProps extends Properties("BoonProps") {
   testable[Char]
   testable[Double]
 
+  defineAssertion[Int]
+  defineAssertion[String]
+  defineAssertion[Char]
+  defineAssertion[Double]
+
   private def testable[A: Arbitrary : BoonType](implicit typeTag: TypeTag[A]): Unit =  {
     val typeName = typeOf[A].toString
     property(s"${typeName}.testable") = Prop.forAll(testableProp[A] _)
   }
 
+  private def defineAssertion[A: Arbitrary : BoonType](implicit typeTag: TypeTag[A]): Unit =  {
+    val typeName = typeOf[A].toString
+    property(s"${typeName}.defineAssertion") = Prop.forAll(assertionProp[A] _)
+  }
+
   private def testableProp[A: BoonType](v1: A, v2: A, eqt: EqualityType): Prop = {
     val testable = Boon.testable[A](defer(v1), defer(v2), eqt).run
+    assertTestable(testable, v1, v2, eqt)
+  }
+
+  private def assertTestable[A: BoonType](testable: Testable, v1: A, v2: A, eqt: EqualityType): Prop = {
     val value1 = testable.value1.run
     val value2 = testable.value2.run
     val rep = StringRep[A].strRep _
@@ -37,5 +52,14 @@ object BoonProps extends Properties("BoonProps") {
     (testable.equalityType == eqt)   :| "equality type"          &&
     (testable.equality.eql(value1, value2) == (v1 == v2 )) :| "equality"  &&
     (testable.difference.diff(value1, value2) == one(s"${rep(v1)} != ${rep(v2)}")) :| "diff"
+  }
+
+  private def assertionProp[A: BoonType](name: String, v1: A, v2: A, eqt: EqualityType, context: Map[String, String]): Prop = {
+    val assertion = Boon.defineAssertion[A](name, (defer(v1), defer(v2)), eqt, context)
+
+    (assertion.name.value == name)                      :| "name"     &&
+    (assertion.context == context)                      :| "context"  &&
+    assertTestable(assertion.testable.run, v1, v2, eqt) :| "testable" &&
+    (assertion.location.line == 58)                     :| "location"
   }
 }
