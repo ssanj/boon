3cbb713149d46bfc6c785a246e23e798e14b8ff9
sanjiv sahayam <sanjsmailbox@gmail.com>
2019-04-17 22:33:00 +1000

    Add props for toList

--- core/src/test/scala/boon/NonEmptySeqProps.scala
+++ core/src/test/scala/boon/NonEmptySeqProps.scala
@@ -10,15 +10,30 @@ import scala.reflect.runtime.universe._
 object NonEmptySeqProps extends Properties("NonEmptySeq") {
 
   mapLaws[String, Int]
-  mapLaws[Int, String]
-  mapLaws[Char, String]
   mapLaws[String, Double]
+  mapLaws[Int, String]
+  mapLaws[Double, Char]
+
+  toListLaws[String]
+  toListLaws[Int]
+  toListLaws[Double]
+  toListLaws[Char]
+
+  private def toListLaws[A: Arbitrary](implicit typeTagA: TypeTag[A]): Unit = {
+    val typeAName = typeOf[A].toString
+    property(s"toList[${typeAName}]") = Prop.forAll(toList[A] _)
+  }
 
   private def mapLaws[A: Arbitrary : Cogen, B: Arbitrary](implicit typeTagA: TypeTag[A], typeTagB: TypeTag[B]): Unit = {
     val typeAName = typeOf[A].toString
     val typeBName = typeOf[B].toString
 
     property(s"map(${typeAName} => ${typeBName})") = Prop.forAll(mappingOnList[A, B] _)
+  }
+
+   //converting to a List is the same as appending the `head` to the tail as a List
+   private def toList[A](inputs: NonEmptySeq[A]): Boolean = {
+    inputs.toList == (inputs.head :: inputs.tail.toList)
   }
 
   //mapping on List is the same as mapping on NES
