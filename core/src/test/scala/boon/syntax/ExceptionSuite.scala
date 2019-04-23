f45df74e727ae91d55179c6ed52e1f4dd7904bf6
sanjiv sahayam <sanjsmailbox@gmail.com>
2019-04-24 00:28:17 +1000

    Add test for exception instance

--- core/src/test/scala/boon/syntax/ExceptionSuite.scala
+++ core/src/test/scala/boon/syntax/ExceptionSuite.scala
@@ -3,32 +3,21 @@ package syntax
 
 import exception._
 
-object ExceptionSuite2 extends SuiteLike("ExceptionSuite2") {
+object ExceptionSuite extends SuiteLike("Exception Syntax Suite") {
 
-  private class Flakey() {
-    def blow: Int = throw new RuntimeException("boom!")
-    def blowNested: String = throw new RuntimeException("nested boom!", new IllegalArgumentException("This is illegal"))
-    def safe: Boolean = true
-  }
-
-  private val t1 = test("NumberFormatException") {
+  private val t1 = test("Exception Thrown: NumberFormatException") {
     "abcd".toInt =!=[NumberFormatException](_ =?= """For input string: "abcd"""" |
       "number format error")
   }
 
-  private val t2 = test("NoSuchElementException") {
+  private val t2 = test("Exception Thrown: NoSuchElementException") {
     List.empty[String].head =!=[NoSuchElementException](_ =?= "head of empty list" |
       "head on empty List")
   }
 
-  private val t3 = test("Flakey") {
-    val flakey = new Flakey()
-
-    flakey.blow  =!=[RuntimeException](_ =?=  "boom!" |
-      "throw RuntimeException") and
-    flakey.blowNested =!=[RuntimeException](_.contains("boom!") |
-      "nested.exception.message") and
-    flakey.safe | "safe does not throw"
+  private val t3 = test("Exception Instance") {
+    val ex = new UnsupportedOperationException("blurb")
+    ex =!=[UnsupportedOperationException](_ =?= "blurb" | "exception instance")
   }
 
   override val tests = oneOrMore(t1, t2, t3)
