1ed775add30bec05c20f31b06f032c6ccda1d55b
sanjiv sahayam <sanjsmailbox@gmail.com>
2019-05-11 23:13:59 +1000

    Add tests for TestState

---
+++ core/src/test/scala/boon/TestStateProps.scala
@@ -0,0 +1,55 @@
+package boon
+
+import boon.model.TestResult
+import boon.model.TestState
+import org.scalacheck.Properties
+import org.scalacheck._
+import org.scalacheck.Prop.BooleanOperators
+import scalacheck.ModelArb._
+
+object TestStateProps extends Properties("TestState") {
+
+  private case class OnlySuccessfulTestResult(testResult: TestResult)
+  private case class OnlyUnsuccessfulTestResult(testResult: TestResult)
+  private case class OnlyIgnoredTestResult(testResult: TestResult)
+
+  property("Failed when any assertion has failed or thrown") =
+    Prop.forAll { (tr: OnlyUnsuccessfulTestResult) =>
+      val testState  = TestResult.testResultToTestState(tr.testResult)
+      (testState == TestState.Failed) :| s"Expected TestState to be 'Failed' but got: '${testState}'"
+    }
+
+  property("Passed when all assertions have passed") =
+    Prop.forAll { (tr: OnlySuccessfulTestResult) =>
+      val testState  = TestResult.testResultToTestState(tr.testResult)
+      (testState == TestState.Passed) :| s"Expected TestState to be 'Passed' but got: '${testState}'"
+    }
+
+  property("Ignored when a test is ignored") =
+    Prop.forAll { (tr: OnlyIgnoredTestResult) =>
+      val testState  = TestResult.testResultToTestState(tr.testResult)
+      (testState == TestState.Ignored) :| s"Expected TestState to be 'Ignored' but got: '${testState}'"
+    }
+
+  private implicit val onlySuccessfulTestResultArbitrary: Arbitrary[OnlySuccessfulTestResult] = Arbitrary {
+    onlySuccessfulTestResultGen.map(OnlySuccessfulTestResult)
+  }
+
+  private implicit val onlyUnsuccessfulSuiteResultArbitrary: Arbitrary[OnlyUnsuccessfulTestResult] = Arbitrary {
+    onlyUnsuccessfulTestResultGen.map(OnlyUnsuccessfulTestResult)
+  }
+
+  private implicit val onlyIgnoredSuiteResultArbitrary: Arbitrary[OnlyIgnoredTestResult] = Arbitrary {
+    testIgnoredResultGen.map(OnlyIgnoredTestResult)
+  }
+
+  private def onlyUnsuccessfulTestResultGen: Gen[TestResult] =
+    Gen.oneOf(testThrewResultGen,
+             compositeTestStoppedOnFirstTestResultGen,
+             singleTestAllFailedTestResultGen)
+
+  private def onlySuccessfulTestResultGen: Gen[TestResult] =
+    Gen.oneOf(compositeTestAllPassedTestResultGen,
+             singleTestAllPassedTestResultGen)
+
+}
