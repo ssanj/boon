e4a49285ac8194376e8389ab3bc9bdbe740f588b
sanjiv sahayam <sanjsmailbox@gmail.com>
2019-06-09 23:53:31 +1000

    Add test for Sum type BoonType derivation

--- core/src/test/scala/boon/CustomClassesSuite.scala
+++ core/src/test/scala/boon/CustomClassesSuite.scala
@@ -2,15 +2,16 @@ package boon
 
 object CustomClassesSuite extends SuiteLike("CustomClasses") {
 
-  final case class Name(value: String)
-  final case class Age(value: Int)
-  final case class Person(name: Name, age: Age)
+  private val t1 = test("default TC instance for Product types") {
 
-  private implicit val ageBoonType    = BoonType.defaults[Age]
-  private implicit val nameBoonType   = BoonType.defaults[Name]
-  private implicit val personBoonType = BoonType.caseClass[Person]
+    final case class Name(value: String)
+    final case class Age(value: Int)
+    final case class Person(name: Name, age: Age)
 
-  private val t1 = test("use default TC instances") {
+    implicit val ageBoonType    = BoonType.defaults[Age]
+    implicit val nameBoonType   = BoonType.defaults[Name]
+    implicit val personBoonType = BoonType.caseClass[Person]
+
 
     val p1 = Person(Name("Royd Eris"), Age(30))
     val p2 = Person(Name("Royd Eris"), Age(30))
@@ -26,7 +27,30 @@ object CustomClassesSuite extends SuiteLike("CustomClasses") {
     p2 =/= p3 | "Person instances with different data are not equal" and
     a1 =?= a2 | "The same ages are equal" and
     n1 =?= n2 | "The same names are equal"
-   }
+ }
 
-  override val tests = one(t1)
+ private val t2 = test("default TC instance for Sum type") {
+   sealed trait Colour
+   case object Red extends Colour
+   case object Green extends Colour
+   case object Blue extends Colour
+
+   implicit val colourBoonType: BoonType[Colour] = BoonType.defaults[Colour]
+
+   val red: Colour   = Red
+   val green: Colour = Green
+   val blue: Colour  = Blue
+
+   red =?= red     | "red is red"        and
+   red =/= green   | "red is not green"  and
+   red =/= blue    | "red is not blue"   and
+   green =/= red   | "green is not red"  and
+   green =/= blue  | "green is not blue" and
+   green =?= green | "green is green"    and
+   blue =/= red    | "blue is not red"   and
+   blue =/= green  | "blue is not green" and
+   blue =?= blue   | "blue is blue"
+ }
+
+  override val tests = oneOrMore(t1, t2)
 }
