0bd25e37600138d01c8df6c0ab754775f7b10e3f
sanjiv sahayam <sanjsmailbox@gmail.com>
2019-06-22 00:35:47 +1000

    Add syntax to equate multiple types

--- core/src/main/scala/boon/syntax/equal.scala
+++ core/src/main/scala/boon/syntax/equal.scala
@@ -1,6 +1,7 @@
 package boon
 package syntax
 
+import boon.model.StringRep
 import model.AssertionData
 
 object equal {
@@ -10,4 +11,18 @@ object equal {
 
   def isDiff[A: BoonType](value1: => A)(implicit loc: SourceLocation): A => AssertionData = 
     value2 =>  value1 =/= value2 | s"${value1.strRep} is different to ${value2.strRep}"
+
+  trait DualTypeEquality[A, B] {
+    def =?=(f: (A,B) => Boolean)(implicit AS: StringRep[A], BS: StringRep[B]): PredicateSyntax
+  }
+
+  implicit class DualTypeEqualitySyntax[A](valueA: => A) { 
+    def =>=[B](valueB: => B) = new DualTypeEquality[A, B] {
+      def =?=(f:(A, B) => Boolean)(implicit AS: StringRep[A], BS: StringRep[B]): PredicateSyntax = new PredicateSyntax {
+        override def |(name: => String, ctx: (String, String)*)(implicit loc: SourceLocation): AssertionData = {
+          f(valueA, valueB) >> (one(s"${valueA.strRep} can't be equated to ${valueB.strRep}"), Replace) | (name, ctx:_*)
+        }
+      }
+    }
+  }
 }
