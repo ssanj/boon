fd7be2e64d6eeaa808ca04d75d97cc13311e7284
sanjiv sahayam <sanjsmailbox@gmail.com>
2021-01-13 18:13:36 +1100

    Remove silencer

--- core/src/main/scala/boon/syntax/types.scala
+++ core/src/main/scala/boon/syntax/types.scala
@@ -2,17 +2,16 @@ package boon
 package syntax
 
 import boon.model.AssertionData
-import com.github.ghik.silencer.silent
 
 object types {
 
-  @silent("never used")
   def =:=[A, B](f: => AssertionData)(implicit ev: A =:= B): AssertionData = {
+    val _ = ev
     f
   }
 
-  @silent("never used")
   def <:<[A, B](f: => AssertionData)(implicit ev: A <:< B): AssertionData = {
+    val _ = ev
     f
   }
 }
